e07608ca3b3dd007d50583ce91aec3cc6187b903
Froze562 <159714409+Froze562@users.noreply.github.com>
2024-11-19 21:08:00 +0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,33 @@
+var countDownDate = new Date("nov 28, 2024 00:00:00").getTime();
+var x = setInterval(function(){
+     var now = new Date().getTime();
+     var distance = countDownDate - now;
+
+     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+     document.getElementById("days").innerHTML = days;
+     document.getElementById("hours").innerHTML = hours;
+     document.getElementById("minutes").innerHTML = minutes;
+     document.getElementById("seconds").innerHTML = seconds;
+
+     if (distance < 0){
+          clearInterval(x);
+          document.getElementById("days").innerHTML = "00";
+          document.getElementById("hours").innerHTML = "00";
+          document.getElementById("minutes").innerHTML = "00";
+          document.getElementById("seconds").innerHTML = "00";
+     }
+},1000);
+$('.dealsOfDay').slick({
+     dots: true,
+     arrows: false,
+     infinite: true,
+     autoplay: true,
+     autoplaySpeed: 4000,
+     speed: 2000,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+});
